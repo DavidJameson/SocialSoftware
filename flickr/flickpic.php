7a785601bf44fc49b4eddea40240b46ce65397a4
Kimberly Eng <kimberly.eng@purchase.edu>
2013-11-25 00:21:57 -0500

    replace with working copy

---
+++ flickr/flickpic.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+	<title>Flickr Collage! </title>
+
+<style type="text/css">
+	body{
+		width: 700px;
+		margin: 0 auto;
+		margin-top: 100px;
+		margin-bottom: 50px;
+
+
+	}
+
+</style>
+
+<script src="/bower_components/jquery/index.js"></script>
+
+<script>
+$('#pic').photosetGrid();
+</script>
+
+</head>
+<body>
+
+<?php 
+
+$topic = $_GET['topic'];
+$topic = str_replace(" ", "+", $topic);
+
+
+$params = array(
+	'api_key' => "dd6b66141b99d4022ec8683456193d21",
+	'format' => 'json',
+	'method' => 'flickr.photos.search', 
+	'per_page' => '16',
+	'tags' => $topic,
+	);
+
+$encoded_params = array();
+
+foreach($params as $k => $v){
+	$encoded_params[] = urlencode($k).'='.urlencode($v);
+}
+
+#$url = "http://api.flickr.com/services/rest/?".implode('&', $encoded_params);
+$url = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=dd6b66141b99d4022ec8683456193d21&tags='.$topic.'&per_page=16&format=json';
+$rsp = file_get_contents($url);
+
+#remove prefix
+
+$rsp = str_replace("jsonFlickrApi(", "", $rsp);
+$rsp = substr($rsp, 0, -1);
+
+
+$rsp_obj = json_decode($rsp,true);
+//echo var_dump($rsp_obj);
+
+//$rsp_obj = array_slice();
+/*
+foreach ($rsp_obj['photos']['photo']['id'] as $var)
+{
+	echo $var.'<br>';
+} */
+$arrayLength = sizeof($rsp_obj['photos']['photo']);
+$arrayLength--;
+
+for ($i = 0;$i <= $arrayLength; $i++)
+{
+	$tempPhotoID = $rsp_obj['photos']['photo'][$i]['id'];
+	$tempPhotoFarm = $rsp_obj['photos']['photo'][$i]['farm'];
+	$tempPhotoServer = $rsp_obj['photos']['photo'][$i]['server'];
+	$tempPhotoSecret = $rsp_obj['photos']['photo'][$i]['secret'];
+
+	echo "<object id='pic'> <img src='http://farm" . $tempPhotoFarm . ".staticflickr.com/" . 
+    $tempPhotoServer . "/" . $tempPhotoID."_". $tempPhotoSecret . "_s.jpg' width='150px' border='0'/> </object>"; 
+
+}
+
+
+
+ ?>
+
+</body>
+</html>
