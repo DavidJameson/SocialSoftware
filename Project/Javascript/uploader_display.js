9117d7c0c265e52bd82e73ebe3ff56dd1787c288
Laerte Sousa <sousa.lae@gmail.com>
2013-11-30 14:46:20 -0500

    upload preview working, but still needs styling. next work on server side upload.

--- Project/Javascript/uploader_display.js
+++ Project/Javascript/uploader_display.js
@@ -1,17 +1,56 @@
-function readURL(input) {
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-		            
-		reader.onload = function (e) {
-			var src = e.target.result;
-			var width = 150;
-			var height = 150;
-			var tag = "<img src='"+src+"' width="+width+"height="+height+"/>";
-			$('#blah').replaceWith(tag);
-		}
-		reader.readAsDataURL(input.files[0]);
-	}
+//when document is fully loaded (Jquery))
+//add event listeners
+$(document).ready(function()
+{
+	window.URL = window.URL || window.webkitURL;
+	
+	var fileSelect = document.getElementById("fileSelect");
+	var fileInput = document.getElementById("fileInput");
+	var fileList = document.getElementById('fileList');
+	
+	fileInput.addEventListener("change",function (e)
+	{
+		handleFiles(fileInput.files);
+	},false);
+	
+	fileSelect.addEventListener("click", function (e) 
+	{
+	  if (fileInput) 
+	  {
+	    fileInput.click();
+	  }
+	  e.preventDefault(); // prevent navigation to "#"
+	}, false);
+	
+		
+});
+//Display preview of files to be uploaded
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  } else {
+  	fileList.innerHTML = '';
+    var list = document.createElement("ul");
+    for (var i = 0; i < files.length; i++) {
+      var li = document.createElement("li");
+      list.appendChild(li);
+      
+      var img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      img.onload = function(e) {
+        window.URL.revokeObjectURL(this.src);
+      }
+      li.appendChild(img);
+      
+      var info = document.createElement("span");
+      info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+      li.appendChild(info);
+    }
+    fileList.appendChild(list);
+  }
 }
+//using Jquery
 function readMultipleURL(input)
 {
 	var length = input.files.length;
@@ -34,6 +73,29 @@ function readMultipleURL(input)
 			}
 			reader.readAsDataURL(input.files[i]);
 		}
+		return true;
+	}
+	else
+	{
+		return false;
 	}
 }
+//using Javascript alone
+function getFile()
+{
+	var selected_file = document.getElementById('input').files[0];
+	
+	//var reader = new FileReader();
+	/*
+	reader.onload = function(e)
+	{
+		var src = e.target.result;
+		alert(src);
+	}
+	
+	reader.readAsDataURL(selected_file);*/
+}
+
+
+
     
