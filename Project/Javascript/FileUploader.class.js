b4457e609237ac7671c50b63e49e9676a2a319b8
Laerte Sousa Neto <sousa.lae@gmail.com>
2013-12-03 14:21:11 -0500

    working on progressbar

--- Project/Javascript/FileUploader.class.js
+++ Project/Javascript/FileUploader.class.js
@@ -4,6 +4,7 @@ function FileUploader(fileInput,fileSelect,displayBox)
 	this.fileSelect = fileSelect;
 	this.displayBox = displayBox;
 	var dropbox;
+	var progressBar;
 	
 	addEvents();
 	
@@ -18,27 +19,54 @@ function FileUploader(fileInput,fileSelect,displayBox)
 		
 		function dragenter(e) 
 		{
-		  e.stopPropagation();
-		  e.preventDefault();
+		 	e.stopPropagation();
+			e.preventDefault();
 		}
 
 		function dragover(e) 
 		{
-		  e.stopPropagation();
-		  e.preventDefault();
+			e.stopPropagation();
+			e.preventDefault();
 		}
-		function drop(e) {
-		  e.stopPropagation();
-		  e.preventDefault();
+		function drop(e) 
+		{
+			e.stopPropagation();
+			e.preventDefault();
 
-		  var dt = e.dataTransfer;
-		  var files = dt.files;
-		  alert(files.length);
+			var dt = e.dataTransfer;
+			var files = dt.files;
 			
-		  handleFiles(files);
+			handleFiles(files);          
 		}
 	};
-	
+	function sendFile(file) 
+	{
+			var uri = "PHP/serverUploader.php";
+            var xhr = new XMLHttpRequest();
+            var fd = new FormData();
+            
+            xhr.open("POST", uri, true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // Handle response.
+                    //alert(xhr.responseText); // handle response.
+                }
+            };
+			
+			xhr.upload.addEventListener("progress", function(e) 
+			{
+		  	  if (e.lengthComputable) 
+				{
+		  		  	var percentage = Math.round((e.loaded * 100) / e.total);
+					progressBar.innerHTML = percentage;
+					alert(percentage);
+				}
+			}, true);
+			
+            fd.append('myFile', file);
+            // Initiate a multipart/form-data upload
+            xhr.send(fd);
+	}
 	//Inside Functions
 	//These functions are private, therefore can only be called from inside.
 	function addEvents()
@@ -56,22 +84,25 @@ function FileUploader(fileInput,fileSelect,displayBox)
 		{
 			handleFiles(fileInput.files);
 		},false);
-	
 	}
 	
 	function handleFiles(files) 
 	{
-	  processCompleted = false;
-	  if(isFileSelected(files)) 
-	  {
-	  	clearDisplayBox();
-		displaySelectedFiles(files);
-	  }
-	  else
-	  {
-	  	displayBox.innerHTML = "<p>No files selected!</p>";
-	  }
-	  return processCompleted;
+		processCompleted = false;
+		if(isFileSelected(files)) 
+		{
+			clearDisplayBox();
+			displaySelectedFiles(files);
+			for(var i =0; i < files.length; i++)
+			{
+				setTimeout(sendFile(files[i]),40000);
+			}
+		}
+		else
+		{
+			displayBox.innerHTML = "<p>No files selected!</p>";
+		}
+		return processCompleted;
 	}
 	function isFileSelected(files)
 	{
@@ -88,29 +119,36 @@ function FileUploader(fileInput,fileSelect,displayBox)
 	}
 	function displaySelectedFiles(files)
 	{
-		var list = document.createElement("ul");
+		var container = document.createElement("div");
 	    for (var i = 0; i < files.length; i++) 
 		{
-	      var li = document.createElement("li");
-	      list.appendChild(li);
-	      
-	      var img = document.createElement("img");
-		  img.className = "images";
-	      img.src = window.URL.createObjectURL(files[i]);
-	      img.height = 60;
-		  
-	      img.onload = function(e) 
-		  {
-	        window.URL.revokeObjectURL(this.src);
-	      }
-		  
-	      li.appendChild(img);
-	      
-	      var info = document.createElement("span");
-		  info.className = "title";
-	      info.innerHTML = files[i].name;
-	      li.appendChild(info);
-	    }
-	    displayBox.appendChild(list);
+		    var content = document.createElement("div");
+			content.className = "displayContent";
+		    container.appendChild(content);
+		      
+		    var img = document.createElement("img");
+			img.className = "images";
+		    img.src = window.URL.createObjectURL(files[i]);
+		    img.height = 60;
+			  
+		    img.onload = function(e) 
+			{
+		      window.URL.revokeObjectURL(this.src);
+		    }
+			  
+		    content.appendChild(img);
+		     
+		    var info = document.createElement("span");
+			info.className = "title";
+		    info.innerHTML = files[i].name;
+			content.appendChild(info);
+			
+			progressBar = document.createElement("span");
+			progressBar.innerHTML = 0;
+			info.className ="progressBar";
+			content.appendChild(progressBar);
+		    
+		}
+	    displayBox.appendChild(container);
 	}
 }
