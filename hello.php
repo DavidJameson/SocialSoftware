0eb5a45fe3e22f32f4d27d5d372ffea3d9990da8
Laerte Sousa Neto <sousa.lae@gmail.com>
2013-12-02 19:06:42 -0500

    Revert "don't need those files."
    This reverts commit 90eb84e60f251406a128c9dbe117b68ef419a124.

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?
+   require 'math.php'; 
+   echo "Hello, PHP\n";
+
+   $answer = Add(2, 3);
+   echo "The answer is $answer\n";
+
+   $answer = Add(4,5);
+   echo "The next answer is $answer\n";
+   
+   $answer = Add(1,8);
+   echo "The third answer is $answer\n";
+?>
---
+++ testMultiply.php
@@ -0,0 +1,7 @@
+<?
+   require 'math.php'; 
+   echo "Test our multiplication function\n";
+
+   $answer = Multiply(2, 3);
+   echo "The answer is $answer\n";
+?>
