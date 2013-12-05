51f298464b835785126d2b4bccabd98b4651f8e3
Laerte Sousa Neto <sousa.lae@gmail.com>
2013-12-04 22:06:14 -0500

    Server upload works.
    -Renamed file in order to be better suitable to its function
    -Added conditions:
         +Check if file already exists
         +Check if file has any errors
         +Check if file is actually an image of extension jpg, jpeg, or png

---
+++ Project/PHP/ImageServerUploader.php
@@ -0,0 +1,70 @@
+<?php
+	$file = ($_FILES['myFile']);
+	$upload_dir = "../uploads/";
+	
+	uploadImageFile($file,$upload_dir);
+	
+	function uploadImageFile($file,$dir)
+	{
+		$ext = getFileExtension($file['name']);
+		
+		if(isImageFile($ext) && noFileError($file) && noDuplicate($file,$dir))
+		{
+			move_uploaded_file($file['tmp_name'],$dir.$file['name']);
+		}
+		elseif(!isImageFile($ext))
+		{
+			echo "not an image file.";
+		}
+		elseif(!noFileError($file))
+		{
+			echo "there's a file error."."<br/>".$file['error'];
+		}
+		elseif(!noDuplicate($file,$dir))
+		{
+			echo "File: ".removeFileExtension($file['name'])."\nAlready exists.";
+		}
+	}
+	function removeFileExtension($file_name)
+	{
+		$temp = explode(".", $file_name);
+		$extension = $temp[0];
+		return $extension;
+	}
+	function getFileExtension($file_name)
+	{
+		$temp = explode(".", $file_name);
+		$extension = end($temp);
+		return $extension;
+	}
+	function isImageFile($extension)
+	{
+		$isImageFile = false;
+		$allowedExts = array("gif", "jpeg", "jpg", "png","JPG");
+		
+		if(in_array($extension, $allowedExts))
+		{
+			$isImageFile = true;
+		}
+		
+		return $isImageFile;
+	}
+	function noFileError($file)
+	{
+		$noError = true;
+		if ($file["error"] > 0)
+		{
+			$noError = true;
+		}
+		return $noError;
+	}
+	function noDuplicate($file,$dir)
+	{
+		$noDuplicate = true;
+		if (file_exists($dir . $file["name"]))
+		{
+			$noDuplicate = false;
+		}
+		return $noDuplicate;
+	}
+?>
