ba624d311159be0dc73baebf74b348fac6629bd2
Laerte Sousa Neto <sousa.lae@gmail.com>
2013-12-10 01:17:57 -0500

    New Object ImageServerUpload
    created a separte object to handle server side uploads, and update information

---
+++ Project/PHP/ImageServerUpload.class.php
@@ -0,0 +1,152 @@
+<?php
+	require 'Credential.class.php'; //stores host info
+	require 'Database.class.php';
+	class ImageServerUpload
+	{
+		
+		private $credential;
+		private $database;
+		private $user;
+		private $file;
+		private $directory;
+		private $image_id;
+		
+		function __construct($user,$file)
+		{
+			$this->credential = new Credential();
+			$this->database = new Database
+								(
+								$this->credential->getHost(),
+								$this->credential->getUsername(),
+								$this->credential->getPassword(),
+								$this->credential->getDBName()
+								);
+								
+			$this->user = $user;
+			$this->file = $file;
+			$this->image_id = uniqid();
+			$this->directory = $this->generateDirectory();
+		}
+		public function toString()
+		{
+			$str = "";
+			$str .=$this->image_id."<br/>";
+			$str .=$this->file['name'].'<br/>';
+			$str .=$this->directory.'<br/>';
+			$str .=$this->user.'<br/>';
+			return $str;
+		}
+		public function uploadImageFile()
+		{
+			$ext = $this->getFileExtension($this->file['name']);
+			
+			if($this->isImageFile($ext) && $this->noFileError() && $this->noDuplicate())
+			{
+				if(move_uploaded_file($this->file['tmp_name'],$this->directory))
+				{
+					$this->database
+					->insertImage($this->image_id,$this->file['name'],$this->user,
+					$this->directory,'not description yet',0);
+					
+					echo "File Uploaded";
+				}
+			}
+			elseif(!$this->isImageFile($ext))
+			{
+				echo "not an image file.";
+			}
+			elseif(!$this->noFileError())
+			{
+				echo "there's a file error."."<br/>".$file['error'];
+			}
+			elseif(!$this->noDuplicate())
+			{
+				echo "Already exists";
+			}
+		}
+		private function removeFileExtension()
+		{
+			$temp = explode(".", $this->file['name']);
+			$extension = $temp[0];
+			return $extension;
+		}
+		private function getFileExtension()
+		{
+			$temp = explode(".", $this->file['name']);
+			$extension = end($temp);
+			return $extension;
+		}
+		private function isImageFile()
+		{
+			$extension = $this->getFileExtension();
+			$isImageFile = false;
+			$allowedExts = array("gif", "jpeg", "jpg", "png","JPG");
+			
+			if(in_array($extension, $allowedExts))
+			{
+				$isImageFile = true;
+			}
+			
+			return $isImageFile;
+		}
+		private function noFileError()
+		{
+			$noError = true;
+			if ($this->file["error"] > 0)
+			{
+				$noError = true;
+			}
+			return $noError;
+		}
+		private function noDuplicate()
+		{
+			$noDuplicate = true;
+			if (file_exists($this->directory))
+			{
+				$noDuplicate = false;
+			}
+			return $noDuplicate;
+		}
+		public function generateDirectory()
+		{
+			$dir = '..'.$this->database->getUserHome($this->user).'/'.
+			$this->image_id."_".$this->file['name'];
+			return $dir;
+		}
+		//Getters and Setters
+		public function getCredential()
+		{
+			return $this->credential;
+		}
+		public function getDatabase()
+		{
+			return $this->database;
+		}
+		public function getFile()
+		{
+			return $this->file;
+		}
+		public function getImage()
+		{
+			return $this->image_id;
+		}
+		public function getDirectory()
+		{
+			return $this->directory;
+		}
+		
+		public function setCredential($credential)
+		{
+			$this->credential = $credential;
+		}
+		public function setDatabase($database)
+		{
+			$this->database = $database;
+		}
+		public function setFile($file)
+		{
+			$this->file = $file;
+		}
+		
+	}
+?>
--- Project/PHP/ImageServerUploader.php
+++ Project/PHP/ImageServerUploader.php
@@ -1,87 +1,12 @@
 <?php
-	require 'Credential.class.php'; //stores host info
-	require 'Database.class.php';
+
+	require 'ImageServerUpload.class.php';
+	
 	session_start();
 	$user = $_SESSION['usr'];
-	$credential = new Credential();
-	$hostAddress = $credential->getHost();
-	$username = $credential->getUsername();
-	$password = $credential->getPassword();
-	$databaseName = $credential->getDBName();
-	
-	$database = new Database($hostAddress,$username,$password,$databaseName);
-	$database->connect();	
-	
-	
-	$file = ($_FILES['myFile']);
-	$upload_dir = '..'.$database->getUserHome($user).'/';
 
-	uploadImageFile($file,$upload_dir);
-	
-	function uploadImageFile($file,$dir)
-	{
-		$ext = getFileExtension($file['name']);
-		
-		if(isImageFile($ext) && noFileError($file) && noDuplicate($file,$dir))
-		{
-			if(move_uploaded_file($file['tmp_name'],$dir.$file['name']))
-			{
-				echo "File Uploaded";
-			}
-		}
-		elseif(!isImageFile($ext))
-		{
-			echo "not an image file.";
-		}
-		elseif(!noFileError($file))
-		{
-			echo "there's a file error."."<br/>".$file['error'];
-		}
-		elseif(!noDuplicate($file,$dir))
-		{
-			echo "Already exists";
-		}
-	}
-	function removeFileExtension($file_name)
-	{
-		$temp = explode(".", $file_name);
-		$extension = $temp[0];
-		return $extension;
-	}
-	function getFileExtension($file_name)
-	{
-		$temp = explode(".", $file_name);
-		$extension = end($temp);
-		return $extension;
-	}
-	function isImageFile($extension)
-	{
-		$isImageFile = false;
-		$allowedExts = array("gif", "jpeg", "jpg", "png","JPG");
-		
-		if(in_array($extension, $allowedExts))
-		{
-			$isImageFile = true;
-		}
-		
-		return $isImageFile;
-	}
-	function noFileError($file)
-	{
-		$noError = true;
-		if ($file["error"] > 0)
-		{
-			$noError = true;
-		}
-		return $noError;
-	}
-	function noDuplicate($file,$dir)
-	{
-		$noDuplicate = true;
-		if (file_exists($dir . $file["name"]))
-		{
-			$noDuplicate = false;
-		}
-		return $noDuplicate;
-	}
+	$file = ($_FILES['myFile']);
+
+	$uploader = new ImageServerUpload($user,$file);
+	$uploader->uploadImageFile();	
 ?>
